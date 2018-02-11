add09c6fb7d509f5fac585fb904443cf52e7a061
Håken Lid <haakenlid@gmail.com>
2018-02-11 21:26:14 +0100

    don't use random input for test suit....

--- src/stories/story.js
+++ src/stories/story.js
@@ -9,10 +9,12 @@ const sentences = n => loremHipsum({ count: n, units: 'sentences' })
 const words = n => loremHipsum({ count: n, units: 'words' })
 
 const baseProps = {
-  host: 'vg',
-  title: words(5),
-  description: sentences(1),
-  content: sentences(5),
+  host: 'db',
+  title: 'Tumblr Williamsburg normcore asymmetrical street art',
+  description:
+    'Sriracha meggings artisan jean shorts cliche brunch Helvetica tote bag vinyl.',
+  content:
+    'Thundercats lomo art party tofu retro pork belly. Sustainable Schlitz asymmetrical craft beer kitsch tofu gluten-free pour-over banh mi tousled artisan cliche trust fund Cosby sweater Odd Future. Fingerstache mustache iPhone normcore banh mi pug lo-fi craft beer Vice mustache fixie pop-up.',
   url: 'https://example.com/story',
   posted: '1973-05-06',
   image: placeholderImage,
